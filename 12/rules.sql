d6bf85a748f09ffa11749a89349aa92ce0346ce9
Logan <xackery@gmail.com>
2019-02-01 12:50:50 -0800

    Update rules.sql

--- 12/rules.sql
+++ 12/rules.sql
@@ -1,3 +1,3 @@
 # Level 75 cap until Secrets of Feydwer
-UPDATE rule_values SET rule_value='70' WHERE rule_name='Character:MaxExpLevel';
-UPDATE rule_values SET rule_value='70' WHERE rule_name='Character:MaxLevel';
+UPDATE rule_values SET rule_value='75' WHERE rule_name='Character:MaxExpLevel';
+UPDATE rule_values SET rule_value='75' WHERE rule_name='Character:MaxLevel';
