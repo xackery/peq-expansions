4660c29307cdf1de54da4c9a82e8b0f5805e7e97
Logan <xackery@gmail.com>
2019-02-01 12:54:44 -0800

    Create rules.sql

---
+++ 18/rules.sql
@@ -0,0 +1,3 @@
+# Level 95 cap until Rain of Fear
+UPDATE rule_values SET rule_value='95' WHERE rule_name='Character:MaxExpLevel';
+UPDATE rule_values SET rule_value='95' WHERE rule_name='Character:MaxLevel';
