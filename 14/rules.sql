e4b203ceb532c72a781684f4bd4bedec3fbfe883
Logan <xackery@gmail.com>
2019-02-01 12:51:34 -0800

    Create rules.sql

---
+++ 14/rules.sql
@@ -0,0 +1,3 @@
+# Level 80 cap until Seeds of Destruction
+UPDATE rule_values SET rule_value='80' WHERE rule_name='Character:MaxExpLevel';
+UPDATE rule_values SET rule_value='80' WHERE rule_name='Character:MaxLevel';
