6bd38e6ac5433907dea95d90f94b5ec3da667f09
Logan <xackery@gmail.com>
2019-02-01 12:52:04 -0800

    Create rules.sql

---
+++ 15/rules.sql
@@ -0,0 +1,3 @@
+# Level 85 cap until House of Thule
+UPDATE rule_values SET rule_value='85' WHERE rule_name='Character:MaxExpLevel';
+UPDATE rule_values SET rule_value='85' WHERE rule_name='Character:MaxLevel';
