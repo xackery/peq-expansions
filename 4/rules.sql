48bd95425293a7c1576b2215980c4d75079bc7c5
Logan <xackery@gmail.com>
2019-02-01 12:47:22 -0800

    Create rules.sql

---
+++ 4/rules.sql
@@ -0,0 +1,3 @@
+# Level 65 cap until Omens of War
+UPDATE rule_values SET rule_value='65' WHERE rule_name='Character:MaxExpLevel';
+UPDATE rule_values SET rule_value='65' WHERE rule_name='Character:MaxLevel';
