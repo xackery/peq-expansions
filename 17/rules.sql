dce6c8f8442041d6f7ab63adc57d36c8f31f11b6
Logan <xackery@gmail.com>
2019-02-01 12:53:46 -0800

    Create rules.sql

---
+++ 17/rules.sql
@@ -0,0 +1,3 @@
+# Level 90 cap until Veil of Alaris
+UPDATE rule_values SET rule_value='90' WHERE rule_name='Character:MaxExpLevel';
+UPDATE rule_values SET rule_value='90' WHERE rule_name='Character:MaxLevel';
