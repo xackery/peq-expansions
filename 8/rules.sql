c28deb3852c7598291be8312de4a116927a84839
Logan <xackery@gmail.com>
2019-02-01 12:49:46 -0800

    Create rules.sql

---
+++ 8/rules.sql
@@ -0,0 +1,3 @@
+# Level 70 cap until The Serpent's Spine
+UPDATE rule_values SET rule_value='70' WHERE rule_name='Character:MaxExpLevel';
+UPDATE rule_values SET rule_value='70' WHERE rule_name='Character:MaxLevel';
