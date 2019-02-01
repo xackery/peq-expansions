443666ea1d40fc358d1d8611f71544f243261649
Logan <xackery@gmail.com>
2019-02-01 12:50:37 -0800

    Create rules.sql

---
+++ 12/rules.sql
@@ -0,0 +1,3 @@
+# Level 75 cap until Secrets of Feydwer
+UPDATE rule_values SET rule_value='70' WHERE rule_name='Character:MaxExpLevel';
+UPDATE rule_values SET rule_value='70' WHERE rule_name='Character:MaxLevel';
