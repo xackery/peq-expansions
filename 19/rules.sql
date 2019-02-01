6873f1c6373d4e9a2341c22bbc489df7871e2abc
Logan <xackery@gmail.com>
2019-02-01 12:55:17 -0800

    Create rules.sql

---
+++ 19/rules.sql
@@ -0,0 +1,3 @@
+# Level 100 cap until The Darkened Sea
+UPDATE rule_values SET rule_value='100' WHERE rule_name='Character:MaxExpLevel';
+UPDATE rule_values SET rule_value='100' WHERE rule_name='Character:MaxLevel';
