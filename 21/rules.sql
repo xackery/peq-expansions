a268fd829ed077e8ed88df966dad23861bf62494
Logan <xackery@gmail.com>
2019-02-20 13:39:47 -0800

    Create rules.sql

---
+++ 21/rules.sql
@@ -0,0 +1,3 @@
+# Level 105 cap until Ring of Scale
+UPDATE rule_values SET rule_value='105' WHERE rule_name='Character:MaxExpLevel';
+UPDATE rule_values SET rule_value='105' WHERE rule_name='Character:MaxLevel';
