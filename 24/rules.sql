d5412fd4cd08d5ca8ace884ba88d56783a850419
Logan <xackery@gmail.com>
2019-02-20 13:40:34 -0800

    Create rules.sql

---
+++ 24/rules.sql
@@ -0,0 +1,3 @@
+# Level 110 cap until
+UPDATE rule_values SET rule_value='110' WHERE rule_name='Character:MaxExpLevel';
+UPDATE rule_values SET rule_value='110' WHERE rule_name='Character:MaxLevel';
