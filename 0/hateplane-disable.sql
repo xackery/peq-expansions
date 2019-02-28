7c5fb5caadad88bb85e75a30d2cb658b425a2083
Logan <xackery@gmail.com>
2019-02-28 12:47:07 -0800

    Update hateplane-disable.sql

--- 0/hateplane-disable.sql
+++ 0/hateplane-disable.sql
@@ -1,2 +1,2 @@
-# Disables hateplane
+# Disables hateplane until Kunark. Set min_expansion to a higher value if you want to disable it until that expansion releases.
 UPDATE zone SET min_expansion=1 WHERE shortname='hateplane';
